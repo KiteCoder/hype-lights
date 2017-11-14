d61ee6a6ed73287148cdaf7c544abc99a0db7027
jeff <JEFF@VAREX.COM>
2017-11-14 13:34:37 -0700

    git ingotre and postgres
    Converted to posgres db

--- app/settings.py
+++ app/settings.py
@@ -77,11 +77,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'hype',
+        'USER': 'postgres',
+        'PASSWORD':'password',
+        'HOST':'localhost',
+
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
