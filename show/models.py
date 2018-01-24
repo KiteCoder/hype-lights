1e2778cd8bafaae0f7130ff0cb19f65cecc1e24d
Cameron <cameron.pattberg@gmail.com>
2018-01-23 19:52:24 -0700

    more changes the the models for the database.

--- show/models.py
+++ show/models.py
@@ -17,18 +17,26 @@ class Show(models.Model):
     name = models.TextField(null=False, blank=False)
     frequency = models.IntegerField(null=False, blank=False)
 
+class Second(models.Model):
+    class Meta:
+        unique_together = ('show', 'second')
+
+    show = models.ForeignKey('Show', on_delete=models.CASCADE, null=False, blank=False)
+    second = models.IntegerField(null=False, blank=False)
+
 class Pixel(models.Model):
+    venue = models.ForeignKey('Venue', on_delete=models.PROTECT, null=False, blank=False)
     row = models.IntegerField(null=False, blank=False)
     seat = models.IntegerField(null=False, blank=False)
     section = models.CharField(null=False, blank=False, max_length=2)
 
-class ShowPixel(models.Model):
+class SecondPixel(models.Model):
     class Meta:
-        unique_together = ('pixel','show')
+        unique_together = ('pixel','second')
 
     pixel = models.ForeignKey('Pixel', on_delete=models.PROTECT, null=False, blank=False)
-    show = models.ForeignKey('Show', null=False, blank=False)
-    array = models.TextField(null=False, blank=False)
+    second = models.ForeignKey('Second', on_delete=models.CASCADE, null=False, blank=False)
+    state = models.TextField(null=False, blank=False)
 
 class ContactPerson(models.Model):
     firstName = models.TextField(null=False, blank=False)
