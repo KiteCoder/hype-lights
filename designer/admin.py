704861f4a1d12c2565e85ebee6d3f75aaa6bad90
Cameron <cameron.pattberg@gmail.com>
2018-01-27 10:44:14 -0700

    registering the admin site.

--- designer/admin.py
+++ designer/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from hypertron.models import Venue, Show, Second, Pixel, SecondPixel, ContactPerson
 
 # Register your models here.
+admin.site.register(Venue)
+admin.site.register(Show)
+admin.site.register(Second)
+admin.site.register(Pixel)
+admin.site.register(SecondPixel)
+admin.site.register(ContactPerson)
