c10e5789fc71f00a17ec861bf75911051e0af40c
Benson Weeks <bensonweeks@gmail.com>
2018-01-30 15:08:59 -0700

    left out a /

--- app/settings.py
+++ app/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -55,7 +55,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 
-LOGIN_REDIRECT_URL = 'account/index'
+LOGIN_REDIRECT_URL = '/account/index'
 
 ROOT_URLCONF = 'app.urls'
 
